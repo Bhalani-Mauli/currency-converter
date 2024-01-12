d0f98305eb548bb30c6b56544ba069d3dec20cd1
Bhalani-Mauli <maulibhalani13@gmail.com>
2024-01-12 22:40:01 +0100

    create a post api

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import indexRoute from "./src/routes/convert.routes";
+import bodyParser from "body-parser";
 
 //For env File
 dotenv.config();
@@ -9,11 +11,15 @@ const app = express();
 const port = process.env.PORT || 8000;
 const projectName: string = "currency-converter";
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.locals.appTitle = projectName;
 app.use(express.static("public"));
+app.use(cors());
 
 // Routes
-app.use("/fe", express.static("../public"));
+app.use("/fe", express.static("public"));
 app.use("/api/convert", indexRoute);
 
 app.listen(port, () => {
